a14517e810ef9ef0700d8a548ff0acf6f90728b2
SafnasKaldeen <110978617+SafnasKaldeen@users.noreply.github.com>
2024-06-28 22:46:18 +0530

    ai voice

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,7 @@ module.exports = {
       primary: "#9333EA",
       secondary: "#ff7e33",
       info: "#0C63E7",
+      Bg: "sky-500",
       black: colors.black,
       white: colors.white,
       slate: colors.slate,
